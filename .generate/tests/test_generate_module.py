3cf49cb7fd084706b9bf05f068a3c26e7c9f4448
Kevin Kortum <kevin.kortum@gmx.de>
2022-06-03 15:27:37 +0200

    info: Add shameful commentary

--- .generate/tests/test_generate_module.py
+++ .generate/tests/test_generate_module.py
@@ -1,5 +1,29 @@
 import generate_module
 
+# TODO: Implement all tests
+# Yep, i am too lazy to do all the mocking for reading files
+# and loading files from URL
 
-def test_():
+
+def test_generate_service_list_from_html():
+    pass
+
+
+def test_write_service_list_cache():
+    pass
+
+
+def test_load_service_list_cache():
+    pass
+
+
+def test_generate_actions_from_service():
+    pass
+
+
+def test_generate_from_cookiecutter():
+    pass
+
+
+def test_main():
     pass
